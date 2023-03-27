93bafed86d7e854eba6d0f660a6ddc0afded4ee1
Moris-Polanco <116950929+Moris-Polanco@users.noreply.github.com>
2023-03-27 15:18:45 -0600

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import requests
+
+# Reemplaza 'tu_clave_api' con la clave API real que obtuviste de Perplexity.AI
+clave_api = 'tu_clave_api'
+
+def obtener_respuesta(pregunta):
+    url = 'https://api.perplexity.ai/v1/dialogue'
+
+    headers = {
+        'Authorization': f'Bearer {clave_api}',
+        'Content-Type': 'application/json',
+    }
+
+    data = {
+        'input': {
+            'text': pregunta,
+            'domain': 'es',  # Establecer el dominio del lenguaje en español
+        },
+        'context': {
+            'knowledge': [
+                {
+                    'text': 'Este chatbot responde preguntas sobre la legislación guatemalteca.',
+                }
+            ]
+        }
+    }
+
+    respuesta = requests.post(url, json=data, headers=headers)
+
+    if respuesta.status_code == 200:
+        return respuesta.json()['output']['text']
+    else:
+        return "Lo siento, no pude obtener una respuesta."
+
+st.title("Chatbot de Legislación Guatemalteca")
+st.write("Escribe tu pregunta sobre la legislación guatemaltealteca y el chatbot intentará responderla.")
+
+pregunta_usuario = st.text_input(“Escribe tu pregunta aquí:”)
+
+if st.button(“Enviar pregunta”):
+if pregunta_usuario:
+respuesta_chatbot = obtener_respuesta(pregunta_usuario)
+st.write(f"Respuesta del chatbot: {respuesta_chatbot}")
+else:
+st.warning(“Por favor, ingresa una pregunta antes de presionar el botón.”)
